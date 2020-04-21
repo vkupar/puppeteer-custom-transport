563d49034f58243f47327241ead7c005baeb245c
Valeri Kuparashvili <valeri.developer@gmail.com>
2020-04-21 21:40:56 +0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+class WSTransport {
+    constructor(ws) {
+        this.ws = ws;
+        this.ws.on("message", (message) => {
+            if (this.onmessage) {
+                this.onmessage(message);
+            }
+        });
+
+        this.ws.on("close", () => {
+            if (this.onclose) {
+                this.onclose();
+            }
+        });
+        this.onmessage = null;
+        this.onclose = null;
+    }
+
+    send(message) {
+        this.ws.send(message);
+    }
+
+    close() {
+        this.ws.close();
+    }
+}
+
+wss.on('connection', (ws) => {
+    setTimeout(async () => {
+        const browser = await puppeteer.connect({
+            transport: new WSTransport(ws),
+            defaultViewport: null
+        });
+
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com');
+    }, 2000);
+});
